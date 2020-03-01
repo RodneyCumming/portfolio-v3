f1d28de060d3dc488706ac1a4650ac1b96475703
RodneyCumming <rodneycumming@gmail.com>
2020-03-01 13:14:03 +1300

    temp all global css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "App";
 import { HashRouter } from "react-router-dom";
 // import "Styles/sanitize.css";
-import "Styles/globalStyles.css";
-import "Styles/fonts/stylesheet.css";
+// import "Styles/globalStyles.css";
+// import "Styles/fonts/stylesheet.css";
 
 ReactDOM.render(
   <HashRouter>
